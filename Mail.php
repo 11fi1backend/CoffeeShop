1e1bfbeb401b7c0f2593a7ab383bf1008df8af57
11fi1backend <11fi1backend@gmail.com>
2015-06-15 09:08:06 +0200

    added MailTransmitter for sending emails

--- Mail.php
+++ Mail.php
@@ -16,6 +16,16 @@ class Mail
     private $destinationMail;
 
     /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
      * @var PDF
      */
     private $pdfFile;
@@ -23,22 +33,27 @@ class Mail
     /**
      * @param $senderMail
      * @param $destinationMail
+     * @param string $subject
+     * @param string $message
      * @param PDF $pdfFile
      */
-    public function __construct($senderMail, $destinationMail, PDF $pdfFile)
+    public function __construct($senderMail, $destinationMail, $subject, $message, PDF $pdfFile)
     {
-        $this->ensureEmailAreValid($senderMail, $destinationMail);
+        $this->ensureEmailsAreValid($senderMail, $destinationMail);
+        $this->ensureMailTextIsValid($subject, $message);
 
         $this->senderMail = $senderMail;
         $this->destinationMail = $destinationMail;
         $this->pdfFile = $pdfFile;
+        $this->subject = $subject;
+        $this->message = $message;
     }
 
     /**
      * @param $senderMail
      * @param $destinationMail
      */
-    private function ensureEmailAreValid($senderMail, $destinationMail)
+    private function ensureEmailsAreValid($senderMail, $destinationMail)
     {
         if (filter_var($senderMail, FILTER_VALIDATE_EMAIL))
         {
@@ -49,5 +64,62 @@ class Mail
         {
             throw new InvalidArgumentException("Given destinationMail is not a valid email");
         }
+    }
+
+    /**
+     * @param $subject
+     * @param $message
+     */
+    private function ensureMailTextIsValid($subject, $message)
+    {
+        if (!is_string($subject))
+        {
+            throw new InvalidArgumentException("Given subject has to be a string");
+        }
+
+        if (!is_string($message))
+        {
+            throw new InvalidArgumentException("Given message has to be a string");
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getSenderMail()
+    {
+        return $this->senderMail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDestinationMail()
+    {
+        return $this->destinationMail;
+    }
+
+    /**
+     * @return PDF
+     */
+    public function getPdfFile()
+    {
+        return $this->pdfFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
     }
 }
