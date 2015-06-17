7db801f3fbec5cd4de67b8ccfd14fcd3a4bc9b44
Dennis-Bezold <d.bezold@kabelmail.de>
2015-06-17 11:46:07 +0200

    update PDF erzeugen

--- src/PDF.php
+++ src/PDF.php
@@ -1,23 +1,29 @@
 <?php
 
 #include('../fpdf17/fpdf.php');
+include('../prince/prince.php'); 
+
+$prince = new Prince("C:/Program Files (x86)/Prince/Engine/bin/prince");
+$prince -> convert_file_to_file("../src/Rechnung.xml", "Rechnung.pdf");
+
+
+#"../src/Rechnung.xml"
 
 /**
  * 
  */
-class PDF
+/*class PDF
 {
     public function __construct()
     {
-    	$prince = new Prince('../prince/prince.php');
-    	#$prince -> convert_file_to_file("../src/Rechnung.xml", "../src/Rechnung.pdf");
+    	$prince = new Prince("../../Program Files (x86)/Prince/Engine/bin/prince.exe");
+    	$prince -> convert_file("../src/Rechnung.xml");
     	
-		
 		
     }
     
 }
 
 
-
+*/
 ?>
