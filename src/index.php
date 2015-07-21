6cbfab1c1daceef9aad65488f5f67a1ac5e3810a
unknown <d.bezold@A108-03.kbbz.ads>
2015-07-21 11:19:56 +0200

    PDF

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@ require_once('lib/AutoLoader.php');
 
 $te = new \lib\TemplateEngine('template/test.xml', 'template/CoffeeShop/templateOutput.xml');
 
+echo shell_exec('java -jar /etc/coffeeshop/fop-2.0/build/fop.jar -fo /etc/coffeeshop/fop-2.0/examples/fo/basic/images.fo -pdf /etc/coffeeshop/fop-2.0/images.pdf');
+
 $te->render(array('%NAME%' => 'fooBar'));
 
 
